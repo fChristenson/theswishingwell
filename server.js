7cc74da4615ea494d51914cd1f500fb97b9f07a8
Fredrik <fredrik.christenson@gmail.com>
2018-04-04 20:15:23 +0200

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src");
 
-const port = process.env.NODE_PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
